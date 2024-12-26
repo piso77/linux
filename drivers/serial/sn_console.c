754a4e01f29b7344f45063d01f02369226dea891
Paolo Pisati <p.pisati@gmail.com>
2024-12-26 19:52:43 +0100

    Revert "[PATCH] irq-flags: serial: Use the new IRQF_ constants"
    This reverts commit 40663cc7f1c1ccf515d8af9470925a0cb2f59b5d.
    
    Signed-off-by: Paolo Pisati <p.pisati@gmail.com>

--- drivers/serial/sn_console.c
+++ drivers/serial/sn_console.c
@@ -648,7 +648,7 @@ static irqreturn_t sn_sal_interrupt(int irq, void *dev_id, struct pt_regs *regs)
 static int sn_sal_connect_interrupt(struct sn_cons_port *port)
 {
 	if (request_irq(SGI_UART_VECTOR, sn_sal_interrupt,
-			IRQF_DISABLED | IRQF_SHARED,
+			SA_INTERRUPT | SA_SHIRQ,
 			"SAL console driver", port) >= 0) {
 		return SGI_UART_VECTOR;
 	}
